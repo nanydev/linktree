7e313f0f55f9cbf62e28d02588de4c5fc505d26d
NanyDev <the.nany.dev@gmail.com>
2023-09-21 00:33:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adc a imagem light
+    img.setAttribute('src', './assets/nanylight.png')
+  } else {
+
+    // se tiver sem light mode, manter a imagem normal - principal
+    img.setAttribute("src", "./assets/nanysun.png")
+  } 
+}
